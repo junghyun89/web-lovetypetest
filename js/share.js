ed89e7d238d301f1fa7700444eea8afd242898bd
jung89 <104253822+junghyun89@users.noreply.github.com>
2022-05-18 14:33:34 +0900

    Add files via upload

--- js/share.js
+++ js/share.js
@@ -1,37 +1,37 @@
 'use strict';
 
-const url = 'https://junghyun89.github.io/web-lovetypetest/'
+const url = 'https://junghyun89.github.io/web-lovetypetest/';
 
 function setShare(){
   let resultImg = document.querySelector('#resultImg');
   let resultAlt = resultImg.firstElementChild.alt;
-  const shareTitle = '십일간지 연애유형 결과';
+  const shareTitle = '십이간지 연애유형 결과';
   const shareDes = infoList[resultAlt].name;  
-  const shareImage = `${url}img/image-resultAlt.png`;
-  const shareURL = `${url}page.result-resultAlt.html`;
+  const shareImage = `${url}'img/image-'resultAlt'.png'`;
+  const shareURL = `${url}'page/result-'resultAlt'.html'`;
 
   Kakao.Link.sendDefault({
-      objectType: 'feed',
-      content: {
-        title: shareTitle,
-        description: shareDes,
-        imageUrl: shareImage,
+    objectType: 'feed',
+    content: {
+      title: shareTitle,
+      description: shareDes,
+      imageUrl: shareImage,
+      link: {
+        mobileWebUrl: shareURL,
+        webUrl: shareURL
+      },
+    },
+
+    buttons: [
+      {
+        title: '결과확인하기',
         link: {
           mobileWebUrl: shareURL,
-          WebUrl: shareURL
+          webUrl: shareURL,
         },
       },
-      buttons: [
-        {
-          title: '결과 확인하기',
-          link: {
-            mobileWebUrl: shareURL,
-            WebUrl: shareURL,
-          },
-        },
-      ]
+    ]
   });
 }
-
 
 
